9f104080a72babb514790a14556ad72cb0598c79
Kadair Eskandar <kadaireskandar@Kadairs-MacBook-Pro.local>
2023-06-09 19:34:17 -0700

    remove palm

--- prompt.js
+++ prompt.js
@@ -12,15 +12,15 @@ const configuration = new Configuration({
 
 
 
-const { TextServiceClient } = require("@google-ai/generativelanguage");
-const { GoogleAuth } = require("google-auth-library");
+// const { TextServiceClient } = require("@google-ai/generativelanguage");
+// const { GoogleAuth } = require("google-auth-library");
 
 const MODEL_NAME = "models/text-bison-001";
 
 
-const client = new TextServiceClient({
-  authClient: new GoogleAuth().fromAPIKey(process.env.PALM_KEY),
-});
+// const client = new TextServiceClient({
+//   authClient: new GoogleAuth().fromAPIKey(process.env.PALM_KEY),
+// });
 
 
 const openAIModelToPricePer1K = {
@@ -144,63 +144,63 @@ function sleep(time) {
 
 
 
-async function promptPalm(prompt) {
+// async function promptPalm(prompt) {
 
-  let promptString = "";
+//   let promptString = "";
 
-  for (let message of prompt) {
-    promptString += message.role + "\n" + message.content + "\n\n";
-  }
+//   for (let message of prompt) {
+//     promptString += message.role + "\n" + message.content + "\n\n";
+//   }
 
 
-  const stopSequences = [];
+//   const stopSequences = [];
   
-  let answer = await client.generateText({
-    // required, which model to use to generate the result
-    model: MODEL_NAME,
-    // optional, 0.0 always uses the highest-probability result
-    temperature: 0.7,
-    // optional, how many candidate results to generate
-    candidateCount: 1,
-    // optional, number of most probable tokens to consider for generation
-    top_k: 40,
-    // optional, for nucleus sampling decoding strategy
-    top_p: 0.95,
-    // optional, maximum number of output tokens to generate
-    max_output_tokens: 8000,
-    // optional, sequences at which to stop model generation
-    stop_sequences: stopSequences,
-    // optional, safety settings
-    safety_settings: [{"category":"HARM_CATEGORY_DEROGATORY","threshold":1},{"category":"HARM_CATEGORY_TOXICITY","threshold":1},{"category":"HARM_CATEGORY_VIOLENCE","threshold":2},{"category":"HARM_CATEGORY_SEXUAL","threshold":2},{"category":"HARM_CATEGORY_MEDICAL","threshold":2},{"category":"HARM_CATEGORY_DANGEROUS","threshold":2}],
-    prompt: {
-      text: promptString,
-    },
-  });
+//   let answer = await client.generateText({
+//     // required, which model to use to generate the result
+//     model: MODEL_NAME,
+//     // optional, 0.0 always uses the highest-probability result
+//     temperature: 0.7,
+//     // optional, how many candidate results to generate
+//     candidateCount: 1,
+//     // optional, number of most probable tokens to consider for generation
+//     top_k: 40,
+//     // optional, for nucleus sampling decoding strategy
+//     top_p: 0.95,
+//     // optional, maximum number of output tokens to generate
+//     max_output_tokens: 8000,
+//     // optional, sequences at which to stop model generation
+//     stop_sequences: stopSequences,
+//     // optional, safety settings
+//     safety_settings: [{"category":"HARM_CATEGORY_DEROGATORY","threshold":1},{"category":"HARM_CATEGORY_TOXICITY","threshold":1},{"category":"HARM_CATEGORY_VIOLENCE","threshold":2},{"category":"HARM_CATEGORY_SEXUAL","threshold":2},{"category":"HARM_CATEGORY_MEDICAL","threshold":2},{"category":"HARM_CATEGORY_DANGEROUS","threshold":2}],
+//     prompt: {
+//       text: promptString,
+//     },
+//   });
 
-  //console.log(JSON.stringify(answer, null, 2));
-  console.log('out');
+//   //console.log(JSON.stringify(answer, null, 2));
+//   console.log('out');
 
-  const md = answer[0].candidates[0].output;
+//   const md = answer[0].candidates[0].output;
 
-  if (promptString,indexOf("JSON")) {
-    try {
-      md = md.replace(/```json/g, '').replace(/```/g, '');
-      JSON.parse(md);
+//   if (promptString,indexOf("JSON")) {
+//     try {
+//       md = md.replace(/```json/g, '').replace(/```/g, '');
+//       JSON.parse(md);
       
-      return {
-        answer: md
-        }
-    }
-    catch (e) {
-      console.log('Fail JSON PARSE answer');
-      console.log(JSON.stringify(answer, null, 2));
-      return promptPalm(prompt);
-    }
-  }
-  return {
-    answer: md.replace(/```json/g, '').replace(/```/g, '')
-  };
-}
+//       return {
+//         answer: md
+//         }
+//     }
+//     catch (e) {
+//       console.log('Fail JSON PARSE answer');
+//       console.log(JSON.stringify(answer, null, 2));
+//       return promptPalm(prompt);
+//     }
+//   }
+//   return {
+//     answer: md.replace(/```json/g, '').replace(/```/g, '')
+//   };
+// }
   
  
 
